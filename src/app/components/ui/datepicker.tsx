ad9788dc8ddd444d7f70c1adef90c9a0dd1fa4f0
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2025-05-14 11:07:44 -0400

    app form

--- src/app/components/ui/datepicker.tsx
+++ src/app/components/ui/datepicker.tsx
@@ -13,31 +13,34 @@ import {
   PopoverTrigger,
 } from "@/app/components/ui/popover";
 
-export function DatePicker() {
+export function DatePicker({ name }: { name: string }) {
   const [date, setDate] = React.useState<Date>();
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn(
-            "w-[280px] justify-start text-left font-normal",
-            !date && "text-muted-foreground"
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
+    <>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "w-[280px] justify-start text-left font-normal",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+      <input type="hidden" name={name} value={date ? date.toISOString() : ""} />
+    </>
   );
 }
--- src/app/pages/applications/New.tsx
+++ src/app/pages/applications/New.tsx
@@ -1,9 +1,37 @@
 import { InteriorLayout } from "@/app/layouts/InteriorLayout";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/app/components/ui/breadcrumb";
+import { ApplicationsForm } from "@/app/components/ApplicationForm";
+import { db } from "@/db";
 
-const New = () => {
+const New = async () => {
+  const statuses = await db.applicationStatus.findMany();
   return (
     <InteriorLayout>
-      <div>New</div>
+      <div className="mb-12 -mt-7 pl-[120px]">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/applications">Applications</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Add an Application</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <div className="mx-page-side pb-6 mb-8 border-b-1 border-border">
+        <h1 className="page-title">New Application</h1>
+        <p className="page-description">Create a new application</p>
+      </div>
+      <ApplicationsForm statuses={statuses} />
     </InteriorLayout>
   );
 };
